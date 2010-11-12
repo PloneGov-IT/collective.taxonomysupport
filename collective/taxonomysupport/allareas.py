9559d8f30ae3526228966c54c2924f3f21ffcd07
keul <luca@keul.it>
2010-11-12 14:06:27 +0000

    Parziale rever della versione precedente

--- collective/taxonomysupport/allareas.py
+++ collective/taxonomysupport/allareas.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from Products.CMFCore.permissions import View
+from Products.CMFCore.utils import getToolByName
 from AccessControl import ClassSecurityInfo
 
 from Products.Archetypes.atapi import Schema
 from Products.Archetypes.atapi import LinesField
+from Products.Archetypes.atapi import MultiSelectionWidget
 from Products.Archetypes.atapi import StringField
 from Products.Archetypes.atapi import SelectionWidget
-from Products.Archetypes.atapi import LinesWidget
 from Products.Archetypes.atapi import DisplayList
 
 from Products.ATContentTypes.criteria import registerCriterion
@@ -18,6 +19,8 @@ from Products.ATContentTypes.criteria.base import ATBaseCriterion
 from Products.ATContentTypes.criteria.schemata import ATBaseCriterionSchema
 
 from Products.ATContentTypes import ATCTMessageFactory as _
+
+from collective.taxonomysupport.interfaces import IFolderTaxonomy
 
 CompareOperators = DisplayList((
                     ('and', _(u'and'))
@@ -32,10 +35,10 @@ ATAllAreasCriterionSchema = ATBaseCriterionSchema + Schema((
                 accessor="Value",
                 mutator="setValue",
                 default=[],
-                widget=LinesWidget(
-                    label=_(u'label_list_criteria_value', default=u'Values'),
-                    description=_(u'help_list_criteria_value',
-                                  default=u'Values, each on its own line.')
+                vocabulary="getCurrentValues",
+                widget=MultiSelectionWidget(
+                    label=_(u'label_criteria_values', default=u'Values'),
+                    description=_(u'help_criteria_values', default=u'Existing values.')
                     ),
                 ),
     StringField('operator',
@@ -45,8 +48,7 @@ ATAllAreasCriterionSchema = ATBaseCriterionSchema + Schema((
                 default='or',
                 vocabulary=CompareOperators,
                 widget=SelectionWidget(
-                    label=_(u'label_list_criteria_operator',
-                            default=u'operator name'),
+                    label=_(u'label_list_criteria_operator', default=u'operator name'),
                     description=_(u'help_list_criteria_operator',
                                   default=u'Operator used to join the tests on each value.')
                     ),
@@ -54,14 +56,19 @@ ATAllAreasCriterionSchema = ATBaseCriterionSchema + Schema((
     ))
 
 class ATAllAreasCriterion(ATBaseCriterion):
-    """A list criterion on all Taxonomy in the site"""
+    """A selection criterion on all Taxonomy in the site"""
 
     __implements__ = ATBaseCriterion.__implements__ + (IATTopicSearchCriterion, )
     security       = ClassSecurityInfo()
-    schema         = ATAllAreasCriterionSchema
+    schema         = ATSelectionCriterionSchema
     meta_type      = 'ATAllAreasCriterion'
-    archetype_name = 'List all Taxonomy Criterion'
-    shortDesc      = 'List of all site Taxonomy'
+    archetype_name = 'Selection on Taxonomy Criterion'
+    shortDesc      = 'Select values from list'
+
+    def getCurrentValues(self):
+        catalog = getToolByName(self, 'portal_catalog')
+        results = catalog(object_provides=IFolderTaxonomy.__identifier__)
+        return [x.Title for x in results]
 
     security.declareProtected(View, 'getCriteriaItems')
     def getCriteriaItems(self):
@@ -75,4 +82,4 @@ class ATAllAreasCriterion(ATBaseCriterion):
 
         return tuple(result)
 
-registerCriterion(ATAllAreasCriterion, LIST_INDICES)
+registerCriterion(ATSelectionCriterion, LIST_INDICES)
