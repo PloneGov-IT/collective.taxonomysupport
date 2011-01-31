aa87749bdbb6af5afa36e9b910ff9615f3a814ef
cekk <andrea.cecchi85@gmail.com>
2011-01-31 15:28:53 +0000

    Preparing release 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of collective.taxonomysupport
 import os
 from setuptools import setup, find_packages
 
-version = '1.1.0dev'
+version = '1.1.0'
 
 tests_require=['zope.testing']
 
